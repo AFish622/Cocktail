722b9aaf66a8f718d4bee795a4148890f1f82b5e
Austin Fisher <AFish622@gmail.com>
2017-07-19 10:51:05 -0700

    First functional app

--- cocktail.js
+++ cocktail.js
@@ -1,101 +1,89 @@
-const drinksUrl = "http://www.thecocktaildb.com/api/json/v1/1/search.php";
 
-$(function(){
-	waitForClickOnSubmit();
-})
+var getDataFromApi = function(endpoint, query, callback) {
+	$.getJSON('http://www.thecocktaildb.com/api/json/v1/1/' + endpoint + query, function(data){
+		const hasData = data.drinks === null ? noData() : callback(data);
+	})
+}
 
-//Function waiting for user to click submit button.
-function waitForClickOnSubmit() {
+var noData = function(){
+	$('.results').text('No drinks were found, please try search again');
+}
+
+var renderIngredients = function(data) {
+	data.drinks.forEach(function(ingredient){
+		$('.results').append('<li class="appendCocktail"><a class="cocktailLink" href="">' + ingredient.strDrink + '</a></li>')
+	})
+}
+
+const renderDrink = data => {
+	const toAppend = data.drinks.map(function(drink){
+		return '<li class="appendCocktail"><a class="cocktailLink" href="">' + drink.strDrink + '</a></li>';
+	})
+	$('.results').append(toAppend)
+}
+
+var renderIndividualCocktail = function(data){
+	$('.drink-thumbnail, .drink-name, .drink-glass, .drink-instructions').text('');
+	$('.drink-ingredients').text('');
+	$('.drink-template').removeClass('hidden');
+
+	const obj = data.drinks[0];
+
+	console.log(obj)
+
+	const filteredKeys = Object.keys(obj)
+							.filter(key => obj[key]);
+
+	const ingredients = filteredKeys
+							.filter(key => key.includes('strIngredient'))
+							.map(key => obj[key]);
+
+	const measurements = filteredKeys
+	 						.filter(key => key.includes('strMeasure'))
+	 						.map(key => obj[key]);
+
+	const thumbnail = obj.strDrinkThumb
+	const name = obj.strDrink;
+	const glassware = obj.strGlass;
+	const howTo = obj.strInstructions;
+
+	ingredients.forEach((ingredient, i) => {
+		$('.drink-ingredients').append(`<p class="detailed-list">${measurements[i]} ${ingredients[i]}</p>`)
+	})
+	
+	var drinkThumbnail = thumbnail === null ? $('.drink-thumbnail').attr('src', 'https://tabatavaleria.files.wordpress.com/2011/03/bartender02.gif') : $('.drink-thumbnail').attr('src', thumbnail);
+	$('.drink-name').append(name);
+	$('.drink-glass').append(glassware);
+	$('.drink-instructions').append(howTo);
+}
+
+var waitForClickOnSubmit = function() {
 	$('.search-form').submit(function(event){
 		event.preventDefault();
 		const cocktailTarget = $('.main-search').val();
-		$('.main-search').val('')
+		$('.drink-template').addClass('hidden');
+		$('.search-results').removeClass('hidden');
+		$('.main-search').val('');
 		const queryType = $('select').find(':selected').val();
-		const callback = queryType == 'Ingredients' ? getIngredients : getDrink
+		const callback = queryType == 'Ingredients' ? renderIngredients : renderDrink
 		const endpoint = queryType == 'Ingredients' ? 'filter.php?i=' : 'search.php?s='
-		getDataFromApi(endpoint, cocktailTarget, callback)
+		$('.results').text('');
+		$('drink-template').text('');
+		getDataFromApi(endpoint, cocktailTarget, callback);
 	})
 }
 
-function getDataFromApi(endpoint, query, callback) {
-	$.getJSON('http://www.thecocktaildb.com/api/json/v1/1/' + endpoint + query, function(data){
-		callback(data)
-	})
-}
-
-function getIngredients(data) {
-	data.drinks.forEach(function(ingredient){
-	$('.results').append('<li class="appendCocktail">' + ingredient.strDrink + '</li>')
-	})
-	console.log('Hello from ingredients')
-	console.log(data)
-}
-
-function getDrink(data) {
-	data.drinks.forEach(function(drink){
-		$('.results').append('<li class="appendCocktail">' + drink.strDrink + '</li>');
-		console.log(drink.strDrink);
-	})
-	console.log('Hello from drinks')
-	//console.log(data)
-}
-
-// function getIndDrink(obj) {
-// 	const filtered = Object.keys(obj).filter(key => {
-// 		return  key.includes('ingredients') && obj[key]
-// 	})
-
-// 	.map(key => obj[key])
-// }
-
-
-
-//function getIdOfChosenDrink(){
+var getIdOfChosenDrink = function(){
 	$('body').on('click', '.cocktailLink', function(event){
 		event.preventDefault();
 		$('.appendCocktail').addClass('hidden');
-		let chosenCocktail = $(this).text()
-		$.getJSON('http://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + chosenCocktail, function(object){
-			var template = $('#drink-template').html();
-			var drinkItem = object.drinks[0];
-			$(template).find('.drink-name').text(drinkItem.strDrink);
-			$('.detailed-results').append(template);
-		})
+		let chosenCocktail = $(this).text();
+		getDataFromApi('search.php?s=', chosenCocktail, renderIndividualCocktail);
+
 	})
-//}
+}
 
- 
-
-
-//Function to take user input and search API for drink by ID
-// function getCocktailDatafromApi(searchwords) {
-// 	$.getJSON('http://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + searchwords, function(data) {
-// 			data.drinks.forEach(function(drink){
-// 				var currentDrink = drink.idDrink;
-// 				let drinkNumber = drink.idDrink;
-// 				getCocktail(drinkNumber);
-// 				//console.log(drink.idDrink);
-// 				//$('.results').append(drink.idDrink);
-// 			})
-// 	})
-// }
-
-// function getIngredientDataFromApi(searchword) {
-// 	$.getJSON('http://www.thecocktaildb.com/api/json/v1/1/search.php?i=' + searchword, function(data) {
-// 		data.ingredients.forEach(function(ingredient){
-// 			let searchedIngredient = ingredient.strIngredient
-// 			getCocktailIngredients(searchedIngredient);
-// 		})
-// 	})
-// }
-
-// function getCocktail(drinkId) {
-// 	$.getJSON('http://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + drinkId, function(id){
-// 		console.log(id);
-// 		id.drinks.forEach(function(drink){
-// 			let cocktailName = drink.strDrink
-// 			$('.results').append('<li class="appendCocktail"><a class="cocktailLink" href="">' + cocktailName + '</a></li>')
-// 		})
-// 		//getIdOfChosenDrink();
-// 	})
-// }
+$(function(){
+	waitForClickOnSubmit();
+	getIdOfChosenDrink();
+})
